bbac01ce1abec8f648b06689d9c0a14bec9908cd
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-12-12 12:01:54 +0100

    Fix mailer

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
-  config.mailer_sender    = "change-me@domain.org"
+  config.application_name = "Decidim Hospitalet"
+  config.mailer_sender    = "info@codegram.com"
   config.authorization_handlers = [ExampleAuthorizationHandler]
 
   # Uncomment this lines to set your preferred locales
