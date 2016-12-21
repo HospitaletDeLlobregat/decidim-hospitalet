f1578b9566013dff521375bd0d2dd221763d749c
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-12-21 15:51:10 +0100

    Upgrade decidim

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,11 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
