1a599060eeba4233af998bcd8b52d4bbc1fc93bd
Aitor Porro <89.aitor.porro@gmail.com>
2017-01-13 08:36:14 +0100

    Remove Authorization Handler (#21)

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,7 +2,7 @@
 Decidim.configure do |config|
   config.application_name = "Decidim Hospitalet"
   config.mailer_sender    = "info@codegram.com"
-  config.authorization_handlers = [ExampleAuthorizationHandler]
+  config.authorization_handlers = []
 
   # Uncomment this lines to set your preferred locales
   # config.available_locales = %{en ca es}
