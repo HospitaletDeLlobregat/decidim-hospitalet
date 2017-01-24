765a66b2826918a4bd1628b7c196999bf086a09f
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-01-24 09:19:46 +0100

    Fix carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,17 +11,17 @@ end
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage = :aws
-    config.aws_bucket = ENV.fetch('decidim-hospitalet')
-    config.aws_acl    = 'public-read'
+    config.aws_bucket = ENV.fetch("decidim-hospitalet")
+    config.aws_acl    = "public-read"
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
     config.aws_attributes = {
       expires: 1.week.from_now.httpdate,
-      cache_control: 'max-age=604800'
+      cache_control: "max-age=604800"
     }
 
     config.aws_credentials = {
-      access_key_id:     Rails.application.secrets.aws_access_key_id
+      access_key_id:     Rails.application.secrets.aws_access_key_id,
       secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region:            "eu-central-1"
     }
