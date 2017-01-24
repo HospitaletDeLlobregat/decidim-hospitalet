c7e989bbb1481f8a65e18fbfe1ead037fce025ad
Josep Jaume Rey <josepjaume@gmail.com>
2017-01-24 09:31:12 +0100

    Use carrierwave-aws instead of fog (#45)
    * Use carrierwave-aws instead of fog
    
    * Fix carrierwave

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ end
 group :production do
   gem 'rails_12factor'
   gem 'sendgrid-ruby'
-  gem 'fog-aws'
+  gem 'carrierwave-aws'
   gem 'sentry-raven'
   gem 'newrelic_rpm'
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,23 +6,25 @@ CarrierWave.configure do |config|
   config.directory_permissions = 0o777
 end
 
-# Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
+# Setup CarrierWave to use Amazon S3. Add `gem "carrierwave-aws" to your Gemfile.
 #
 if Rails.env.production?
-  require 'carrierwave/storage/fog'
-
   CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'                                             # required
-    config.fog_credentials = {
-      provider:              'AWS',                                             # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,       # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,   # required
-      host:                  "s3.eu-central-1.amazonaws.com",
-      region:                "eu-central-1"                                     # optional, defaults to 'us-east-1'
+    config.storage = :aws
+    config.aws_bucket = ENV.fetch("decidim-hospitalet")
+    config.aws_acl    = "public-read"
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: "max-age=604800"
     }
-    config.fog_directory  = "decidim-hospitalet"                                # required
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
+
+    config.aws_credentials = {
+      access_key_id:     Rails.application.secrets.aws_access_key_id,
+      secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region:            "eu-central-1"
+    }
   end
 else
   CarrierWave.configure do |config|
