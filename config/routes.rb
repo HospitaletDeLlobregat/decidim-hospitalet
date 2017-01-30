e1afb2661971ebbfb1b7e359a1c6dd8f9fb92d52
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-01-30 11:23:50 +0100

    Add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  authenticate :user, lambda { |u| u.roles.include?("admin") } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
