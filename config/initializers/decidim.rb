e50eb9e286b12774185f1194c58fa4d93b75bedd
Josep Jaume Rey <josepjaume@gmail.com>
2017-01-30 11:47:12 +0100

    Add openstreetmap (#80)

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -7,4 +7,11 @@ Decidim.configure do |config|
   # Uncomment this lines to set your preferred locales
   config.available_locales = %i{ca es}
   config.default_locale = :ca
+
+  if Rails.application.secrets.geocoder
+    config.geocoder = {
+      here_app_id: Rails.application.secrets.geocoder["here_app_id"],
+      here_app_code: Rails.application.secrets.geocoder["here_app_code"]
+    }
+  end
 end
