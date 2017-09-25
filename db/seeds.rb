4f15caca8755865a33d5f884236dd3cb1c22c984
David Morcillo <david.morcillo@gmail.com>
2017-09-25 14:17:04 +0200

    Update decidim v0.6.4 (#167)
    * Update decidim to v0.5.1
    
    * Read-only surveys
    
    * Use %i instead of array
    
    * Update decidim to v0.6.4
    
    * Fix specs
    
    * Fix seeds
    
    * Fix scopes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,26 +20,27 @@ participatory_process = Decidim::ParticipatoryProcess.published.promoted.first
 password = SecureRandom.base64(16)
 
 puts "Deleting default L'Hospitalet scopes..."
+Decidim::Scope.where.not(parent_id: nil).where(organization: organization).destroy_all
 organization.scopes.destroy_all
 
 puts "Creating custom L'Hospitalet scopes..."
 
 scopes = [
-  "El Centre",
-  "Sant Josep",
-  "Sanfeliu",
-  "Collblanc",
-  "La Torrassa",
-  "Santa Eulàlia",
-  "Granvia",
-  "La Florida",
-  "Les Planes",
-  "Pubilla Cases",
-  "Can Serra",
-  "Bellvitge",
-  "El Gornal"
+  { ca: "El Centre" },
+  { ca: "Sant Josep" },
+  { ca: "Sanfeliu" },
+  { ca: "Collblanc" },
+  { ca: "La Torrassa" },
+  { ca: "Santa Eulàlia" },
+  { ca: "Granvia" },
+  { ca: "La Florida" },
+  { ca: "Les Planes" },
+  { ca: "Pubilla Cases" },
+  { ca: "Can Serra" },
+  { ca: "Bellvitge" },
+  { ca: "El Gornal" },
 ].map do |scope_name|
-  { name: scope_name, organization: organization }
+  { name: scope_name, organization: organization, code: scope_name[:ca] }
 end
 scopes = Decidim::Scope.create!(scopes)
 
@@ -64,11 +65,11 @@ categories = [
 
 Decidim::Category.transaction do
   categories.each do |data|
-    Decidim::Category.create!(name: data, description: data, participatory_process: participatory_process)
+    Decidim::Category.create!(name: data, description: data, participatory_space: participatory_process)
   end
 end
 
-categories = Decidim::Category.where(participatory_process: participatory_process).pluck(:id)
+categories = Decidim::Category.where(participatory_space: participatory_process).pluck(:id)
 
 Decidim::Feature.where(manifest_name: :hospitalet_surveys).find_each do |feature|
   3.times do
