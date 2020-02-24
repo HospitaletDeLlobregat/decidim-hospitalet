7c62eeed0edbd25704229754cc3391b07d8b67ce
Marc Riera Casals <mrc2407@gmail.com>
2020-02-24 15:39:23 +0100

    Fix specs

--- config/initializers/entities.rb
+++ config/initializers/entities.rb
@@ -1,9 +1,10 @@
 Decidim.menu :menu do |menu|
   page = Decidim::StaticPage.find_by(slug: 'entitats')
-  return unless page
 
-  menu.item I18n.t("menu.entities", scope: "decidim"),
-            decidim.page_path(page),
-            position: 3,
-            active: :inclusive
+  if page
+    menu.item I18n.t("menu.entities", scope: "decidim"),
+              decidim.page_path(page),
+              position: 3,
+              active: :inclusive
+  end
 end
