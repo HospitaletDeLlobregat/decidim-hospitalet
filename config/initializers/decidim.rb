f254500ae2d39fbf9ca40005c40f995421978aa9
Ivan Vergés <ivan@pokecode.net>
2025-03-17 14:54:22 +0100

    from email

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 Decidim.configure do |config|
-  config.application_name = "Decidim Hospitalet"
-  config.mailer_sender = "nocontestar@lhon-participa.cat"
-  config.force_ssl = ENV["FORCE_SSL"].present?
+  config.application_name = Rails.application.secrets.application_name
+  config.mailer_sender = Rails.application.secrets.mailer_sender
 
   # Uncomment this lines to set your preferred locales
-  config.available_locales = [:ca, :es]
-  config.default_locale = :ca
+  config.available_locales = Rails.application.secrets.available_locales
+  config.default_locale = Rails.application.secrets.default_locale
 
   config.maps = {
     provider: :here,
     api_key: Rails.application.secrets.maps[:api_key],
-    static: { url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview" }
+    static: { url: "https://image.maps.hereapi.com/mia/v3/base/mc/overlay" }
   }
   config.geocoder = {
     timeout: 5,
